4cf9124abb99bbdf44e423440b5e62190030d03b
workflow-templates <wangshiwei@yahoo.com>
2017-12-19 20:24:15 +0800

    fix
    fix

--- template/js/index.js
+++ template/js/index.js
@@ -3,7 +3,7 @@ let loading = require('../../common/utils/loading/index');
 {{#analytics}}
 let logger = require('../../common/utils/logger/index');
 {{/analytics}}
-loading.init({title:'Loading...',txt:'});
+loading.init({title:'Loading...',txt:''});
 loading.show();
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
